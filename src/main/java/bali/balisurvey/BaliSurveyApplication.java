392549eefc0c390bc9789e7721ea57280efb1745
gooinsung <gooinsung96@gmail.com>
2025-02-17 21:03:47 +0900

    first commit

---
+++ src/main/java/bali/balisurvey/BaliSurveyApplication.java
@@ -0,0 +1,13 @@
+package bali.balisurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliSurveyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaliSurveyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bali/balisurvey/BaliSurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package bali.balisurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaliSurveyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
